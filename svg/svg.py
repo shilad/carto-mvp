eccffb3c84e31e9268eb6fed399bb05cb974c09f
yurenpang <ypang@macalester.edu>
2019-06-04 16:36:14 -0500

    Draw svg with labels and country label

--- svg/svg.py
+++ svg/svg.py
@@ -1,11 +1,16 @@
-import numpy as np
 import pandas as pd
 import drawSvg as draw
 
 data = pd.read_csv("../Results/final.csv").iloc[:, 1:]
 color = ["red", "blue", "green", "yellow", "brown", "orange", "cyan", "black"]
 
+
 def scale():
+    """
+    Scale the population score using min-max
+    (x-min)/(max-min)
+    :return:
+    """
     scale_lst = []
     max = data.loc[data['pop_score'].idxmax()]['pop_score']
     min = data.loc[data['pop_score'].idxmin()]['pop_score']
@@ -16,11 +21,12 @@ def scale():
 
     data["scale"] = scale_lst
 
-d = draw.Drawing(4000, 4000, origin="center")
-scale()
-pd.set_option('display.max_columns', 500)
 
 def find_boundaries():
+    """Find boundaries of each country
+    :return: a dictionary, key is the country id
+    values are x, y position and label names
+    """
     country = data.groupby(['countries'], as_index=False).median()
     labels = pd.read_csv("../Results/country_labels.csv", header=None)
     labels.columns = ['countries', 'labels']
@@ -30,11 +36,14 @@ def find_boundaries():
     for index, row in z.iterrows():
         dic[row["countries"]] = [row["x"], row["y"], row["labels"]]
 
-    for key, value in dic.items():
-        print(key, " ", value)
     return dic
 
+# Draw initial frame
+d = draw.Drawing(4000, 4000, origin="center")
+scale()
+pd.set_option('display.max_columns', 500)
 
+# Draw circles of each article and labels for each article
 for i in range(4001):
     title = data.iloc[i, 0]
     x = data.iloc[i, 3]*20
@@ -43,13 +52,12 @@ for i in range(4001):
     size = data.iloc[i, 7]*50
     d.append(draw.Circle(x, y, size, fill=color[country]))
     if size > 3:
-        d.append(draw.Text(title, int(size), x, y)) #, style="font-size: 100px"))
+        d.append(draw.Text(title, int(size), x, y))
 
+# Draw labels for each countries
 country_label_dic = find_boundaries()
 for key, value in country_label_dic.items():
     d.append(draw.Text(value[2], 100, value[0]*20, value[1]*20))
 
 d.setPixelScale(2)  # Set number of pixels per geometry unit
 d.saveSvg('example.svg')
-
-
