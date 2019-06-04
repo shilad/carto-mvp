d4c0883231d6a064ad41fd7248c3a804b6048b47
lirvin123 <lily.irvin@gmail.com>
2019-06-04 12:24:09 -0500

    Added vector csv for t-SNE

--- vectors/food-vectors.py
+++ vectors/food-vectors.py
@@ -1,14 +1,14 @@
 import csv
 from collections import defaultdict
-import pandas as pd
 
 vectors = {}
 foodVectors = defaultdict(list)
 
-with open('vectors/original-vectors.txt', encoding="ISO-8859-1") as file:
+with open('vectors/original-vectors', encoding="ISO-8859-1") as file:
     for line in file:
         values = line.split()
         vectors[values[0]] = values[1:]
+print(vectors['Donald_Trump'])
 
 with open('domain-concept/data.csv', encoding="ISO-8859-1") as data:
     foods = [row[0] for row in csv.reader(data, delimiter='\n')]
@@ -22,10 +22,10 @@ for vector in vectors:
 print(foodVectors.keys())
 print(len(foodVectors))
 
-pd_foodVectors = pd.DataFrame(list(foodVectors.items()))
-pd_foodVectors.columns = ['Article', 'Vectors']
-final_vectors = pd_foodVectors.set_index('Article')
-final_vectors.to_csv("vectors/t-SNE_Vectors")
+# pd_foodVectors = pd.DataFrame(list(foodVectors.items()))
+# pd_foodVectors.columns = ['Article', 'Vectors']
+# final_vectors = pd_foodVectors.set_index('Article')
+# final_vectors.to_csv("vectors/t-SNE_Vectors")
 
 
 
