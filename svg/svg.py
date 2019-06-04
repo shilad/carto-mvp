8a788c3bd90520ede53687390fb499fc6a439c73
yurenpang <ypang@macalester.edu>
2019-06-04 16:30:56 -0500

    Draw svg without labels

--- svg/svg.py
+++ svg/svg.py
@@ -16,19 +16,40 @@ def scale():
 
     data["scale"] = scale_lst
 
-d = draw.Drawing(2000, 2000, origin="center")
+d = draw.Drawing(4000, 4000, origin="center")
 scale()
 pd.set_option('display.max_columns', 500)
 
-print(data.head())
-print(data.columns)
+def find_boundaries():
+    country = data.groupby(['countries'], as_index=False).median()
+    labels = pd.read_csv("../Results/country_labels.csv", header=None)
+    labels.columns = ['countries', 'labels']
+    z = country.merge(labels)
+
+    dic = {}
+    for index, row in z.iterrows():
+        dic[row["countries"]] = [row["x"], row["y"], row["labels"]]
+
+    for key, value in dic.items():
+        print(key, " ", value)
+    return dic
+
+
 for i in range(4001):
-    x = data.iloc[i, 3]*8
-    y = data.iloc[i, 4]*8
+    title = data.iloc[i, 0]
+    x = data.iloc[i, 3]*20
+    y = data.iloc[i, 4]*20
     country = data.iloc[i, 5]
     size = data.iloc[i, 7]*50
     d.append(draw.Circle(x, y, size, fill=color[country]))
+    if size > 3:
+        d.append(draw.Text(title, int(size), x, y)) #, style="font-size: 100px"))
+
+country_label_dic = find_boundaries()
+for key, value in country_label_dic.items():
+    d.append(draw.Text(value[2], 100, value[0]*20, value[1]*20))
 
 d.setPixelScale(2)  # Set number of pixels per geometry unit
 d.saveSvg('example.svg')
+
 
